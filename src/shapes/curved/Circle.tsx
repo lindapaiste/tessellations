f2bf87921d8db5cbfced0bdd1583416445f4c86a
Linda Paiste <lindapaiste@gmail.com>
2021-05-18 18:44:37 -0500

    change from <circle> to <ellipse>

--- src/shapes/curved/Circle.tsx
+++ src/shapes/curved/Circle.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { ShapeComponent } from "../types";
 
-export const Circle: ShapeComponent = ({ width, center, ...props }) => (
-  <circle {...props} cx={center[0]} cy={center[1]} r={0.5 * width} />
+/**
+ * Implement Circle with <ellipse> rather than <circle>
+ * so that the shape is circular by default,
+ * but custom height arguments are not ignored.
+ */
+export const Circle: ShapeComponent = ({ width, height, center, ...props }) => (
+  <ellipse
+    {...props}
+    cx={center[0]}
+    cy={center[1]}
+    rx={0.5 * width}
+    ry={0.5 * (height ?? width)}
+  />
 );
 Circle.standardHeight = (width) => width;
